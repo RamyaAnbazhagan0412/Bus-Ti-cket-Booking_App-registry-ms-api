36904a0a8ae905ca60161740ce7d0dd314cdc264
RamyaAnbazhagan0412 <aramya0412@gmail.com>
2022-05-05 18:22:24 +0530

    added features

--- src/main/java/com/bus_ticket_booking_api/BusTicketBookingApiRegistryMsApiApplication.java
+++ src/main/java/com/bus_ticket_booking_api/BusTicketBookingApiRegistryMsApiApplication.java
@@ -2,7 +2,9 @@ package com.bus_ticket_booking_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class BusTicketBookingApiRegistryMsApiApplication {
 
