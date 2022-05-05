b85df817a6e2a0a8c93c8fa0216aee219ffcff8d
RamyaAnbazhagan0412 <aramya0412@gmail.com>
2022-05-05 16:49:11 +0530

    added features

---
+++ src/main/java/com/bus_ticket_booking_api/BusTicketBookingApiRegistryMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.bus_ticket_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketBookingApiRegistryMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTicketBookingApiRegistryMsApiApplication.class, args);
+	}
+
+}
